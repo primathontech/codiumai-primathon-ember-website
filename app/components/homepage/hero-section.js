321745a364037a9fa8c18a7ab33e961c5cadb6e3
Ramvishvas Kumar <ramvishvas.kumar@primathon.in>
2021-07-26 21:41:52 +0530

    add poster video controll

--- app/components/homepage/hero-section.js
+++ app/components/homepage/hero-section.js
@@ -5,16 +5,52 @@ import { action } from '@ember/object';
 export default class HomepageHeroSectionComponent extends Component {
   @tracked videoSource = '';
   @tracked showControls = false;
-  @tracked showPlayButton = false;
-  _element = null;
+  @tracked showPlayButton = true;
+  @tracked autoplay = true;
+  videoId = 'POSTER_VIDEO';
   _videoUrl = 'https://prima3d.s3.amazonaws.com/primathon.mp4';
+  _isFullScreen = false;
 
   @action
-  playVideo(element) {
-    this._element = element;
+  playVideo() {
+    if (!this._element) {
+      this._element = document.getElementById(this.videoId);
+    }
     this.videoSource = this._videoUrl;
-    this.showControls = true;
     this.showPlayButton = false;
+  }
+
+  @action
+  addControl() {
+    if (!this._element) {
+      this._element = document.getElementById(this.videoId);
+    }
+    if (!this.showControls) {
+      this.showControls = true;
+    }
+  }
+
+  @action
+  onMetaLoad() {
+    if (this._element.requestFullscreen) {
+      this._element.requestFullscreen();
+    } else if (this._element.mozRequestFullScreen) {
+      this._element.mozRequestFullScreen();
+    } else if (this._element.webkitRequestFullscreen) {
+      this._element.webkitRequestFullscreen();
+    } else if (this._element.msRequestFullscreen) {
+      this._element.msRequestFullscreen();
+    }
+  }
+
+  @action
+  onFullScreenChange() {
+    if (this._isFullScreen) {
+      this._isFullScreen = false;
+      this.onEnded();
+    } else {
+      this._isFullScreen = true;
+    }
   }
 
   @action
