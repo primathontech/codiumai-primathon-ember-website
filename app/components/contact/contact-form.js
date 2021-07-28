fe21c1ad2d9e1fcc3d4aab37a798e30527e73c6c
Ramvishvas Kumar <ramvishvas.kumar@primathon.in>
2021-07-28 17:53:19 +0530

    Add email api

--- app/components/contact/contact-form.js
+++ app/components/contact/contact-form.js
@@ -1,15 +1,21 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ContactContactFormComponent extends Component {
+  @service api;
   @tracked submitted = false; //check if form is submitted
+  @tracked apiInProgress = false;
+  @tracked apiError = false;
+  _COUNTRY_CODE_REGEX = /^(\+?\d{1,3}|\d{1,4})$/;
 
   //user questions
   @tracked userQuestion = {
     firstName: '',
     lastName: '',
     email: '',
+    countryCode: '+91',
     phone: '',
     companyName: '',
     message: '',
@@ -28,9 +34,16 @@ export default class ContactContactFormComponent extends Component {
   //to remove validation message if focus on input
   @action
   resetErrorMessage(name) {
+    if (this.apiError) {
+      this.apiError = false;
+    }
     if (this.validation[name]) {
       this.validation = { ...this.validation, [name]: '' };
     }
+  }
+
+  countryCodeValidator(code) {
+    return this._COUNTRY_CODE_REGEX.test(code);
   }
 
   @action validate() {
@@ -54,10 +67,18 @@ export default class ContactContactFormComponent extends Component {
       validateObj.email = 'Please enter valid email';
       isValidated = false;
     }
-    if ((formObj.phone.length > 10 || formObj.phone.length < 10) && formObj.phone.length !== 0) {
-      validateObj.phone = 'Please enter valid phone number';
-      isValidated = false;
+    if (formObj.phone) {
+      if (!this.countryCodeValidator(formObj.countryCode)) {
+        validateObj.countryCode = 'Please enter valid country code';
+        isValidated = false;
+      }
     }
+
+    // if ((formObj.phone.length > 10 || formObj.phone.length < 10) && formObj.phone.length !== 0) {
+    //   validateObj.phone = 'Please enter valid phone number';
+    //   isValidated = false;
+    // }
+
     if (formObj.companyName.length < 1) {
       validateObj.companyName = 'Please enter valid company name';
       isValidated = false;
@@ -70,19 +91,19 @@ export default class ContactContactFormComponent extends Component {
     return isValidated;
   }
 
-  @action sendMessage() {
+  @action
+  async sendMessage() {
     if (this.validate()) {
-      this.submitted = true;
-      const recipient = 'yashveer@primathon.in';
-      const subject = 'New Sales Enquiry';
-      const body = `Name: ${this.userQuestion.firstName} ${this.userQuestion.lastName}
-      Email: ${this.userQuestion.email} 
-      ${this.userQuestion.phone ? 'Phone: ' + this.userQuestion.phone : ''}
-      Company Name: ${this.userQuestion.companyName} 
+      this.apiInProgress = true;
+      const res = await this.api.sendContactMail(this.userQuestion);
 
-      ${this.userQuestion.message}`;
-
-      window.open(`mailto:${recipient}?subject=${subject}&body=${encodeURIComponent(body)}`);
+      if (res.status === 200) {
+        this.submitted = true;
+        this.apiInProgress = true;
+      } else {
+        this.apiError = true;
+        this.apiInProgress = true;
+      }
     }
   }
 }
---
+++ app/services/api.js
@@ -0,0 +1,30 @@
+import Service from '@ember/service';
+
+export default class ApiService extends Service {
+  _API_HOST = 'https://prima-api.primathon.in';
+
+  async request(url, data) {
+    const res = await fetch(`${this._API_HOST}${url}`, data);
+
+    if (!res.ok) {
+      throw res;
+    }
+
+    return await res.json();
+  }
+
+  requestOptionDefaults(data = {}, headers = {}) {
+    return {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers,
+      },
+      body: JSON.stringify(data),
+    };
+  }
+
+  async sendContactMail(payload) {
+    return await this.request(`/contact`, this.requestOptionDefaults(payload));
+  }
+}
