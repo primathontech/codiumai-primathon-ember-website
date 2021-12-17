36911e5382e843f1472d2695548ceb7c0de7a0e4
Ramvishvas Kumar <ramvishvas.kumar@primathon.in>
2021-12-17 16:26:09 +0530

    Change - Contact us api domain

--- app/services/api.js
+++ app/services/api.js
@@ -1,7 +1,8 @@
 import Service from '@ember/service';
 
 export default class ApiService extends Service {
-  _API_HOST = 'https://prima-api.primathon.in';
+  // _API_HOST = 'https://prima-api.primathon.in';
+  _API_HOST = 'https://rkd48nybcl.execute-api.us-east-1.amazonaws.com/dev';
 
   async request(url, data) {
     const res = await fetch(`${this._API_HOST}${url}`, data);
