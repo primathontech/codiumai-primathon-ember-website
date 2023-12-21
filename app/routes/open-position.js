79d01ac93f96ccb268a437d657a2c9dece94f593
Primathon <aastha.pandey@primathon.in>
2023-12-21 17:54:50 +0530

    Updated Title and UI Fixes

--- app/routes/open-position.js
+++ app/routes/open-position.js
@@ -5,16 +5,16 @@ import { inject as service } from '@ember/service';
 export default class OpenPositionRoute extends Route {
   @service headData;
 
-  async model() {
-    const jobData = await (await fetch('http://blog.primathon.in//wp-json/wp/v2/jobs_posting')).json();
-    return jobData;
-  }
+  // async model() {
+  //   const jobData = await (await fetch('http://blog.primathon.in//wp-json/wp/v2/jobs_posting')).json();
+  //   return jobData;
+  // }
   beforeModel() {
-    const title = 'Best Custom Software Development Company India| Primathon';
+    const title = 'Top Custom Software Development Services | Primathon';
     const description =
       ' If you are looking for the best custom software service India then you have come to the right place. Primathon is the top web application development company Delhi.';
     const keywords =
-      'best custom software development company india custom software application development services web application development services india top web application development company delhi best companies for software engineers gurgaon software application development company noida ';
+      'top software development services india custom software application development services web application development services india top web application development company delhi best companies for software engineers gurgaon software application development company noida ';
 
     this.headData.setData({ title, description, keywords });
   }
--- app/routes/services/staff-augmentation/index.js
+++ app/routes/services/staff-augmentation/index.js
@@ -5,11 +5,11 @@ export default class ServicesStaffAugmentationIndexRoute extends Route {
   @service headData;
 
   beforeModel() {
-    const title = 'Best Custom Software Development Company India| Primathon';
+    const title = 'Top Custom Software Development Services | Primathon';
     const description =
       ' If you are looking for the best custom software service India then you have come to the right place. Primathon is the top web application development company Delhi.';
     const keywords =
-      'best custom software development company india custom software application development services web application development services india top web application development company delhi best companies for software engineers gurgaon software application development company noida ';
+      'top software development services india custom software application development services web application development services india top web application development company delhi best companies for software engineers gurgaon software application development company noida ';
 
     this.headData.setData({ title, description, keywords });
   }
