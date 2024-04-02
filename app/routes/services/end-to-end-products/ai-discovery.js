45c6efec92e3a4ddda10aac1c4f5f67fcbf9a3b3
Zeeshan <zeeshan.alam@primathon.in>
2024-04-02 14:08:48 +0530

    chg: add meta title and desc for ai discovery page

--- app/routes/services/end-to-end-products/ai-discovery.js
+++ app/routes/services/end-to-end-products/ai-discovery.js
@@ -12,9 +12,9 @@ export default class ServicesEndToEndProductsAiChatbotRoute extends Route {
   }
 
   beforeModel() {
-    const title = "Best AI Conversational Chatbots Across India | Primathon";
+    const title = 'Get AI Discovery Service Today | Hire from Primathon';
     const description =
-      "Explore top AI Conversational Chatbots for seamless engagement and business acceleration. Connect with us to elevate your operations with powerful automation.";
+      "Our AI discovery service experts conduct an assessment of your company's operations, products, and objectives to pinpoint potential areas for AI integration.";
 
     this.headData.setData({ title, description });
   }
