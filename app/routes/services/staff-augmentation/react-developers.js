50bca47837ee4f21c805c5469fa8f70bce7a6457
Zeeshan <zeeshan.alam@primathon.in>
2024-04-04 18:51:55 +0530

    chg: chg react js meta title

--- app/routes/services/staff-augmentation/react-developers.js
+++ app/routes/services/staff-augmentation/react-developers.js
@@ -12,12 +12,10 @@ export default class ServicesStaffAugmentationReactDevelopersRoute extends Route
   }
 
   beforeModel() {
-    const title = 'Hire React.js Developers | Contact Primathon';
+    const title = 'Hire React.js Developers | Dedicated & Certified React.js Programmers | Primathon';
     const description =
       'Hire React.js Developers from Primathon for tailored solutions. From bespoke to seamless integration, trust our certified experts. Get started now!';
-    const keywords =
-      'hire professional react.js developers gurgaon react.js development service provider company best react.js development solutions india top react.js developers service companies gurgaon best quality react.js development services ';
 
-    this.headData.setData({ title, description, keywords });
+    this.headData.setData({ title, description });
   }
 }
