f052b4465b8456a27a7924996051d39341a41b5d
Vinesh Wadhwani <vinesh.wadhwani@primathon.in>
2024-04-09 18:39:02 +0530

    [Feature]: Created search-gpt json data

---
+++ app/constants/search-gpt/search-gpt.js
@@ -0,0 +1,362 @@
+export const SEARCH_GPT_DATA = {
+  heroSection: {
+    title1: 'Search GPT',
+    subtitle: 'Get Advanced AI-powered Document Management and Personalization',
+    description:
+      'Streamline your workflow, enhance efficiency, and unlock unparalleled insights with advanced tools tailored to your needs. Transform the way you manage documents with innovative document solutions and propel your business forward with confidence. Why is <br /> <b>Search GPT Better?</b>',
+    listItems: [
+      {
+        id: 1,
+        text: 'Efficient Document Summarization',
+      },
+      {
+        id: 2,
+        text: 'Accurate FAQ Summary Generation',
+      },
+      {
+        id: 3,
+        text: 'Intelligent Personalization',
+      },
+      {
+        id: 4,
+        text: 'Advanced Semantic Understanding',
+      },
+    ],
+    ctaText1: 'Book a Call',
+    ctaText2: 'Get Free Consultation',
+    redirect: '/contact',
+    imageSrc: '/assets/emberjs/hire-ember-herosection-img.png',
+    imageAlt: 'Hire Ember Developer',
+  },
+
+  searchGptDevelopmentServices: {
+    title: 'Discover the Standout Search GPT Features',
+    cardData: [
+      {
+        id: 1,
+        imageSrc: '/assets/vue/web-development.svg',
+        cardTitle: 'FAQ Document Summaries',
+        content:
+          'Search GPT excels in generating concise FAQ summary generation, allowing users to quickly access relevant information without the need to sift through lengthy documents.',
+      },
+      {
+        id: 2,
+        imageSrc: '/assets/emberjs/consulting.svg',
+        cardTitle: 'Comprehensive Document Summarization AI',
+        content:
+          'With advanced AI capabilities, Search GPT can summarize lengthy documents into concise and informative summaries, enabling users to grasp key insights and information efficiently.',
+      },
+      {
+        id: 3,
+        imageSrc: '/assets/emberjs/design.svg',
+        cardTitle: 'Intelligent Document Personalization',
+        content:
+          'Search GPT goes beyond basic summarization by offering intelligent document personalization. It analyzes user preferences and behaviors to tailor content recommendations, ensuring a customized and engaging experience.',
+      },
+      {
+        id: 4,
+        imageSrc: '/assets/emberjs/maintenance.svg',
+        cardTitle: 'Semantic Understanding',
+        content:
+          'Leveraging cutting-edge natural language processing (NLP) techniques, Search GPT possesses a deep semantic understanding of text, allowing for accurate and contextually relevant summarization.',
+      },
+      {
+        id: 5,
+        imageSrc: '/assets/vue/migration-service.svg',
+        cardTitle: 'Multi-Language Support',
+        content:
+          'Search GPT supports multiple languages, enabling users worldwide to access its powerful document management features regardless of language barriers.',
+      },
+      {
+        id: 6,
+        imageSrc: '/assets/vue/migration-service.svg',
+        cardTitle: 'Real-Time Updates',
+        content:
+          'Stay up-to-date with the latest information and developments through real-time updates provided by Search GPT, ensuring that users always have access to the most relevant and current content.',
+      },
+      {
+        id: 7,
+        imageSrc: '/assets/vue/migration-service.svg',
+        cardTitle: 'Ease of Integration',
+        content:
+          'Seamlessly integrate Search GPT into existing workflows and systems with ease, thanks to its flexible and adaptable architecture, minimizing disruption and maximizing productivity.',
+      },
+      {
+        id: 8,
+        imageSrc: '/assets/vue/migration-service.svg',
+        cardTitle: 'Scalability',
+        content:
+          "Whether you're a small startup or a large enterprise, Search GPT is designed to scale with your business needs, providing reliable performance and efficiency as your document management requirements grow.",
+      },
+    ],
+  },
+
+  ctaSection: {
+    title: 'Are You Ready to Transform Your Business?',
+    description: '',
+    ctaText: 'HIRE US',
+    redirect: '/contact',
+  },
+
+  howSearchGptWorks: {
+    title: 'How Search GPT Works ?',
+    cardData: [
+      {
+        id: 1,
+        imageSrc: '/assets/ai-discovery/strategic-AI-roadmap.png',
+        cardTitle: 'AI-Powered Document Processing',
+        cardData:
+          'Being an outstanding AI text summarization, Search GPT utilizes state-of-the-art artificial intelligence (AI) technology to process and analyze documents with unparalleled accuracy and efficiency.',
+      },
+      {
+        id: 2,
+        imageSrc: '/assets/ai-discovery/in-depth-AI-potential.png',
+        cardTitle: 'Machine Learning Document Analysis',
+        cardData:
+          'Through advanced machine learning algorithms, Search GPT learns from vast amounts of data to understand document structures, extract key information, and generate insightful summaries.',
+      },
+      {
+        id: 3,
+        imageSrc: '/assets/ai-discovery/personalized-exploration-session.png',
+        cardTitle: 'Natural Language Processing (NLP) Capabilities',
+        cardData:
+          'Leveraging natural language processing techniques, Search GPT comprehends the nuances of human language, enabling it to extract meaning and context from text with remarkable precision.',
+      },
+      {
+        id: 4,
+        imageSrc: '/assets/ai-discovery/personalized-exploration-session.png',
+        cardTitle: 'Data Parsing Techniques',
+        cardData:
+          'Search GPT employs data parsing techniques to identify relevant information within documents, including key phrases, topics, and entities, ensuring that summaries are comprehensive and informative.',
+      },
+      {
+        id: 5,
+        imageSrc: '/assets/ai-discovery/personalized-exploration-session.png',
+        cardTitle: 'Personalization Algorithms',
+        cardData:
+          'With personalized algorithms, Search GPT tailors document summaries and recommendations based on user preferences, behavior, and interactions, delivering a customized experience for each user.',
+      },
+      {
+        id: 6,
+        imageSrc: '/assets/ai-discovery/personalized-exploration-session.png',
+        cardTitle: 'Continuous Learning',
+        cardData:
+          'Search GPT continually learns and improves over time through feedback loops and data updates, ensuring that its summarization and personalization capabilities remain up-to-date and accurate.',
+      },
+      {
+        id: 7,
+        imageSrc: '/assets/ai-discovery/personalized-exploration-session.png',
+        cardTitle: 'Scalable Infrastructure',
+        cardData:
+          'Built on a scalable infrastructure, Search GPT can handle large volumes of documents and user requests with ease, providing reliable performance and responsiveness even as data volumes grow.',
+      },
+      {
+        id: 8,
+        imageSrc: '/assets/ai-discovery/personalized-exploration-session.png',
+        cardTitle: 'Integration with AI Technologies',
+        cardData:
+          'Search GPT seamlessly integrates with other AI technologies and platforms, allowing businesses to leverage its capabilities within their existing workflows and systems for enhanced efficiency and productivity.',
+      },
+    ],
+  },
+
+  specificationOfsearchGpt: {
+    title: 'Technical Specifications of Search GPT',
+    cardData: [
+      {
+        id: 1,
+        imageSrc: '/assets/vue/web-development.svg',
+        cardTitle: 'Document Format Compatibility',
+        content:
+          'Search GPT is compatible with a wide range of document formats, including PDF, DOCX, TXT, and more, ensuring flexibility and accessibility for users.',
+      },
+      {
+        id: 2,
+        imageSrc: '/assets/emberjs/consulting.svg',
+        cardTitle: 'Language Capabilities',
+        content:
+          'Search GPT supports multiple languages, allowing users to analyze and summarize documents in their preferred language with ease.',
+      },
+      {
+        id: 3,
+        imageSrc: '/assets/emberjs/design.svg',
+        cardTitle: 'System Integration Requirements',
+        content:
+          'To integrate Search GPT into existing systems, minimal system requirements are needed, including stable internet connectivity and compatible hardware configurations.',
+      },
+      {
+        id: 4,
+        imageSrc: '/assets/emberjs/maintenance.svg',
+        cardTitle: 'Scalable Infrastructure',
+        content:
+          'Search GPT is built on a scalable infrastructure that can accommodate growing data volumes and user demands, ensuring reliable performance and responsiveness.',
+      },
+      {
+        id: 5,
+        imageSrc: '/assets/vue/migration-service.svg',
+        cardTitle: 'API Integration',
+        content:
+          'Search GPT offers API integration options, allowing seamless integration with other applications and platforms, enhancing workflow efficiency and productivity.',
+      },
+      {
+        id: 6,
+        imageSrc: '/assets/vue/migration-service.svg',
+        cardTitle: 'Updates and Maintenance',
+        content:
+          'Regular updates and maintenance are conducted to ensure that Search GPT remains up-to-date with the latest technologies and functionalities, delivering optimal performance and user experience.',
+      },
+    ],
+  },
+
+  ctaSection1: {
+    title: 'Trust Primathon To Boost Your AI Development Experience',
+    description:
+      'Let us assist you in transforming your web development journey so that you can experience excellence in AI development. Contact Primathon today to kickstart your Project.',
+    ctaText: 'GET IN TOUCH',
+    redirect: '/contact',
+    bgColor: '#F4F4F4',
+  },
+
+  enhanceDocument: {
+    title: 'Enhance Document Workflow with AI-powered Search GPT',
+    description:
+      'Search GPT integrates AI seamlessly into workflows, automating tasks like long text summarization. It enhances efficiency by swiftly extracting key insights, improving document discovery, and decision-making. With Search GPT, businesses optimize workflows, staying ahead in a fast-paced environment.',
+  },
+
+  aiDocumentProcessing: {
+    title: 'AI Document Processing Security and Compliance',
+    description:
+      'Search GPT prioritizes security and compliance with stringent measures to ensure user privacy and data protection. When it comes to Data Privacy in AI Summarization, our platform employs robust data encryption standards and adheres to global data protection regulations such as GDPR and HIPAA. We are committed to safeguarding sensitive information throughout the document processing journey, providing in our AI-powered solutions. Compliance in AI document processing involves several key aspects',
+    imageSrc: '/assets/ai-discovery/seeing-ai-in-action.png',
+    alt: 'Seeing Ai In Action',
+    listItems: [
+      { id: 1, text: 'Data Privacy' },
+      { id: 2, text: 'Security' },
+      { id: 3, text: 'Transparency' },
+      { id: 4, text: 'Ethical Use' },
+    ],
+  },
+
+  benefitsOfHiringEmber: {
+    title1: 'Industry Application of',
+    colouredTitle: 'Search GPT',
+    title2: 'and Use Cases for Document AI Legal Industry',
+
+    description: '',
+    cardData: [
+      {
+        id: 1,
+        title: 'Legal Industry',
+        description: '',
+        listItems: ['Contract Analysis', 'Case Management', 'Legal Research'],
+        imageSrc: '/assets/angular-js/dedicated-project-manager.png',
+        imageAlt: 'Dedicated Project Manager discussing project',
+      },
+      {
+        id: 2,
+        title: 'Educational Industry',
+        description: '',
+        listItems: ['Grading and Feedback', 'Plagiarism Detection', 'Curriculum Development'],
+        imageSrc: '/assets/angular-js/task-records.png',
+        imageAlt: 'Daily and Monthly Task Records',
+      },
+      {
+        id: 3,
+        title: 'Customer Service Industry',
+        description: '',
+        listItems: ['Email Classification and Routing', 'Sentiment Analysis', 'Knowledge Base Management'],
+        imageSrc: '/assets/emberjs/strategy.png',
+        imageAlt: 'Diverse Talent Pool',
+      },
+    ],
+  },
+
+  featuresOfPrimathonSearchGpt: {
+    title: 'Why Choose Search GPT Solutions Provided by Primathon?',
+    cardData: [
+      {
+        id: '1',
+        cardTitle: 'Cutting-Edge Technology',
+        content:
+          'Powered by outstanding AI and machine learning algorithms, our Search GPT solution offers advanced document processing capabilities that deliver accurate and insightful results. Get exceptional text summarization using gen AI with our services.',
+      },
+      {
+        id: '2',
+        cardTitle: 'Comprehensive Solutions',
+        content:
+          'Search GPT provides a comprehensive suite of features, including document summarization, FAQ summary generation, and personalized content recommendations, catering to diverse document management needs.',
+      },
+      {
+        id: '3',
+        cardTitle: 'Security and Compliance',
+        content:
+          "With robust security measures and compliance with global data protection regulations, Primathon's Search GPT ensures the confidentiality and integrity of user data, prioritizing privacy and compliance.",
+      },
+      {
+        id: '4',
+        cardTitle: 'Ease of Integration',
+        content:
+          'Seamlessly integrate Search GPT into existing workflows and systems with ease, thanks to its flexible and adaptable architecture, minimizing disruption and maximizing productivity.',
+      },
+      {
+        id: '5',
+        cardTitle: 'Scalability and Reliability',
+        content:
+          'Built on a scalable infrastructure, Search GPT can handle large volumes of documents and user requests with reliability and efficiency, ensuring consistent performance even as data volumes grow.',
+      },
+      {
+        id: '6',
+        cardTitle: 'User-Centric Approach',
+        content:
+          "Primathon's Search GPT is designed with the user in mind, offering intuitive interfaces and personalized experiences that enhance usability and satisfaction.",
+      },
+      {
+        id: '7',
+        cardTitle: 'Continuous Innovation',
+        content:
+          'Committed to staying at the forefront of AI technology, Primathon continuously innovates and updates Search GPT to deliver cutting-edge solutions that meet evolving document management needs.',
+      },
+    ],
+  },
+
+  faqs: {
+    title: 'FAQs',
+    cardData: [
+      {
+        id: 1,
+        question: 'What is Search GPT?',
+        answer: [
+          'Search GPT is an AI-powered document management solution that leverages advanced algorithms to summarize documents, generate FAQ summaries, and personalize content.',
+        ],
+      },
+      {
+        id: 2,
+        question: 'How does Search GPT summarize documents?',
+        answer: [
+          'Search GPT uses natural language processing (NLP) techniques to analyze and extract key information from documents, producing concise summaries that capture the essence of the text.',
+        ],
+      },
+      {
+        id: 3,
+        question: 'Can Search GPT handle different document formats?',
+        answer: [
+          'Yes, Search GPT supports various document formats, including PDF, DOCX, TXT, and more, ensuring compatibility with a wide range of file types.',
+        ],
+      },
+      {
+        id: 4,
+        question: 'What languages does Search GPT support?',
+        answer: [
+          'Search GPT supports multiple languages, allowing users to analyze and summarize documents in their preferred language with ease.',
+        ],
+      },
+      {
+        id: 5,
+        question: 'Can Search GPT be integrated with other systems or applications?',
+        answer: [
+          'Yes, Search GPT offers API integration options, allowing seamless integration with other systems or applications to enhance workflow efficiency and productivity.',
+        ],
+      },
+    ],
+  },
+};
