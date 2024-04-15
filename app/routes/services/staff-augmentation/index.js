d52e12e509d1d103abbcc85eb5f527252b10bc32
Zeeshan <zeeshan.alam@primathon.in>
2024-04-15 17:23:42 +0530

    feat: integrate offshore development page

--- app/routes/services/staff-augmentation/index.js
+++ app/routes/services/staff-augmentation/index.js
@@ -1,16 +1,20 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { STAFF_AUGMENTATIONS } from '../../../constants/services/staff-augmentation';
 
 export default class ServicesStaffAugmentationIndexRoute extends Route {
   @service headData;
 
-  beforeModel() {
-    const title = 'Top Custom Software Development Services | Primathon';
-    const description =
-      ' If you are looking for the best custom software service India then you have come to the right place. Primathon is the top web application development company Delhi.';
-    const keywords =
-      'top software development services india custom software application development services web application development services india top web application development company delhi best companies for software engineers gurgaon software application development company noida ';
+  async model() {
+    const data = STAFF_AUGMENTATIONS;
+    return { data };
+  }
 
-    this.headData.setData({ title, description, keywords });
+  beforeModel() {
+    const title = 'Get Top Offshore Development Services | Contact Primathon';
+    const description =
+      'Discover top-tier offshore development services tailored to your needs. Our team offers comprehensive solutions, ensuring cost-effective, high-quality results.';
+
+    this.headData.setData({ title, description });
   }
 }
