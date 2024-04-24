b4bb55bac2fb12e625df17b1094139b378f476ca
Saurav Kumar <saurav.kumar@primathon.in>
2024-04-24 15:03:38 +0530

    [Feature: Creating ai credit scoring]

---
+++ app/constants/solutions/ai-credit-scroing.js
@@ -0,0 +1,184 @@
+export const AI_CREDIT_SCORING = {
+  heroSection: {
+    title: 'Revolutionize Your Customer Engagement with Our AI Conversational Bot',
+    subTitle: 'Boost customer experience and streamline operations with our 24/7 accessible AI Conversational Bots',
+    buttonData: 'Let’s Get Started',
+    redirect: '/contact',
+  },
+
+  featuresOfPrimathonAIConversationalBot: {
+    title: "Discover the Standout Features of Primathon's AI Conversational Bot",
+    cardData: [
+      {
+        id: '1',
+        cardTitle: 'Round-the-Clock Customer Support',
+        content:
+          'Being the best AI conversational bot development company, our expert-designed AI bots offer seamless, hassle-free customer support, ensuring seamless assistance anytime, anywhere.',
+      },
+      {
+        id: '2',
+        cardTitle: 'Multilingual Capabilities for Global Reach',
+        content:
+          'Our AI bots communicate with customers in their preferred language, enhancing accessibility and engagement. Bid farewell to frustrating inquiries and embrace efficient lead captures through our AI Conversational bots.',
+      },
+      {
+        id: '3',
+        cardTitle: 'Tailorable Responses',
+        content:
+          'With Primathon’s AI conversational bots, you can customize responses to align with your brand voice and policies, ensuring consistency and authenticity in every interaction.',
+      },
+      {
+        id: '4',
+        cardTitle: 'Easy Integration',
+        content:
+          "Seamlessly connect Primathon's AI Conversational Bot with CRM, ERP, and other business systems for streamlined operations and enhanced data management capabilities.",
+      },
+      {
+        id: '5',
+        cardTitle: 'Scalable Solutions',
+        content:
+          'Our experts designed AI conversational bots offer scalable solutions tailored to accommodate businesses of any size, ensuring flexibility and adaptability to meet diverse needs and growth trajectories.',
+      },
+      {
+        id: '6',
+        cardTitle: 'Comprehensive Analytics',
+        content:
+          "With our services by your side, you can gain invaluable insights into customer interactions using Primathon's AI chatbot comprehensive analytics, empowering informed decision-making and continuous enhancement strategies.",
+      },
+    ],
+  },
+
+  bulletList: {
+    title: 'Unlock Core Advantages for Your Business with Primathon’s AI Conversational Bot',
+    imageSrc: '/assets/ai-chatbot/bullet-list-img.png',
+    alt: 'Primathon’s AI Conversational Bot',
+    listItems: [
+      { id: 1, text: 'Boost in Customer Satisfaction and Loyalty' },
+      { id: 2, text: 'Significant Reduction in Response Time' },
+      { id: 3, text: 'Cost Efficiency Through Automated Service' },
+      { id: 4, text: 'Deeper Insights into Customer Preferences and Behavior' },
+      { id: 5, text: 'Continuous Evolution with Machine Learning' },
+      { id: 6, text: 'Automation of Routine Inquiries, Freeing Up Human Agents for Complex Issues' },
+    ],
+  },
+
+  implementationAndOperation: {
+    title: 'Check Out a Brief Guide on Implementation and Operation of Our AI Conversational Bot',
+    cardData: [
+      {
+        id: 1,
+        cardTitle: 'Customization to Meet Specific Business Needs',
+        cardData:
+          "We offer tailored AI chatbots to match the unique requirements and preferences of individual businesses. From adapting conversation flows, branding elements, language and tone, to integration with existing systems, and incorporating specific functionalities- our team ensures the chatbot aligns seamlessly with your business's goals.",
+      },
+      {
+        id: 2,
+        cardTitle: 'Seamless Integration Process',
+        cardData:
+          'We have a team of best AI developers that ensures effortless incorporation of the AI chatbot into existing systems, minimizing disruptions and maximizing operational efficiency for businesses.',
+      },
+      {
+        id: 3,
+        cardTitle: 'Training the Bot with Industry-Specific Knowledge',
+        cardData:
+          'Being the trusted AI conversational bot development company, we curate conversational chatbots that have the potential to understand and respond to sector-specific queries accurately.',
+      },
+      {
+        id: 4,
+        cardTitle: 'Deployment Across Preferred Customer Service Channels',
+        cardData:
+          "Primathon's AI chatbot ensures accessibility and consistency in customer interactions across various platforms, enhancing overall engagement and satisfaction.",
+      },
+      {
+        id: 5,
+        cardTitle: 'Real-time Monitoring and Performance Optimization',
+        cardData:
+          'Our conversational Chatbots offer the features of real-time monitoring, allowing businesses to track interactions as they occur and make immediate adjustments to enhance efficiency.',
+      },
+    ],
+  },
+
+  aiConversationalBotUses: {
+    title: 'Common Use Cases for',
+    subTitle: 'AI Conversational Bot Solutions',
+    cardData: [
+      {
+        id: 1,
+        imageSrc: '/assets/ai-chatbot/customer-support.png',
+        cardTitle: 'Provides Instant Customer Support and FAQs',
+        content:
+          'Leveraging NLP, our AI conversational chatbots understand and respond to user inquiries in real-time. Additionally, they streamline access to FAQs by intelligently retrieving relevant answers to resolve customer queries promptly without human intervention.',
+      },
+      {
+        id: 2,
+        imageSrc: '/assets/ai-chatbot/booking-and-order-processing.png',
+        cardTitle: 'Assist with Bookings and Order Processing',
+        content:
+          'Our proficiency in developing AI conversational bots and their seamless integration helps in offering personalized recommendations, handling transactions, and automated tasks seamlessly within the conversation.',
+      },
+      {
+        id: 3,
+        imageSrc: '/assets/ai-chatbot/leads-and-sales-effort.png',
+        cardTitle: 'Generate Leads and Support Sales Efforts',
+        content:
+          'By engaging with website visitors, qualifying leads, and capturing contact information, our AI bots support sales efforts. Moreover, it also provides product information and guides users through the sales process.',
+      },
+      {
+        id: 4,
+        imageSrc: '/assets/ai-chatbot/quality-improvement.png',
+        cardTitle: 'Gather User Feedback for Quality Improvement',
+        content:
+          'By initiating surveys or prompting users for feedback during conversations, our AI Chatbots analyze responses, and identify areas for improvement. This data-driven approach allows businesses to continuously refine their products or services based on user input.',
+      },
+      {
+        id: 5,
+        imageSrc: '/assets/ai-chatbot/product-recommendation.png',
+        cardTitle: 'Offer Personalized Product Recommendations',
+        content:
+          'By analyzing user preferences, purchase history, and browsing behavior, our AI Conversational Bots engage users in conversations to understand their needs and interests, then suggest relevant products or services tailored to individual preferences.',
+      },
+    ],
+  },
+
+  faqs: {
+    title: 'FAQs',
+    cardData: [
+      {
+        id: 1,
+        question: 'How does the AI conversational bot improve customer engagement?',
+        answer: [
+          'Our AI conversational bot engages customers in real-time conversations, providing instant support and information, thereby enhancing overall engagement and satisfaction.',
+        ],
+      },
+      {
+        id: 2,
+        question: 'Is the setup of the AI conversational bot complex?',
+        answer: [
+          ' Not at all! Our AI conversational bot comes with an easy setup process, requiring minimal technical expertise. We provide comprehensive documentation and support to ensure a smooth implementation.',
+        ],
+      },
+      {
+        id: 3,
+        question: 'Can the AI conversational bot be customized to suit our specific business needs?',
+        answer: [
+          'Absolutely! Our AI conversational bot is highly customizable, allowing you to tailor its responses, design, and functionality according to your unique business requirements.',
+        ],
+      },
+      {
+        id: 4,
+        question: 'How often does the AI conversational bot require maintenance?',
+        answer: [
+          'Our AI conversational bot is designed to be low-maintenance. Regular updates and maintenance are handled automatically, ensuring optimal performance and reliability without interrupting your operations.',
+        ],
+      },
+    ],
+  },
+
+  areYouReady: {
+    title: 'Are you Ready to Optimize Customer Engagements and Streamline Operations?',
+    description:
+      'Take the Next Step and Contact Us to Explore Best AI Conversational Bot for Your Business. Let’s Schedule a Live Demo for You at Your Earliest Convenience',
+    ctaText: 'Contact Us',
+    ctaRoute: 'contact',
+  },
+};
