dd92c2e9be11ad0959bdcea95b56f59412b3484e
Vinesh Wadhwani <vinesh.wadhwani@primathon.in>
2024-05-03 15:20:21 +0530

    [Feature]: Created AI strategy page

---
+++ app/constants/services/ai-hub/ai-strategy/index.js
@@ -0,0 +1,244 @@
+export const AI_STRATEGY = {
+  heroSection: {
+    gradientTitle: 'AI Strategy',
+    subtitle: 'Explore Our AI Strategy Consulting Services to Craft Your Organization’s AI Roadmap for Tomorrow',
+    description:
+      'Embark on a transformative journey with our AI strategy consulting services, designed to empower your organization with cutting-edge genAI solutions. At Primathon, we specialize in helping businesses of all sizes leverage the power of AI to drive innovation, efficiency, and growth. Our expert consultants work closely with your team to understand your unique business objectives and challenges, crafting a tailored AI roadmap that aligns with the goals of your business. At Primathon, we assist companies with a variety of AI solutions, including : ',
+    listItems: [
+      {
+        id: 1,
+        text: 'AI Strategic Planning',
+      },
+      {
+        id: 2,
+        text: 'AI Deployment Strategies',
+      },
+      {
+        id: 3,
+        text: 'Corporate AI Integration',
+      },
+      {
+        id: 4,
+        text: 'AI Governance Models',
+      },
+      {
+        id: 5,
+        text: 'AI Leadership Development',
+      },
+      {
+        id: 6,
+        text: 'Market Trends Analysis',
+      },
+      {
+        id: 7,
+        text: 'Regulatory Compliance',
+      },
+      {
+        id: 8,
+        text: 'AI Ethical Considerations',
+      },
+    ],
+    ctaText1: 'Book a Call',
+    ctaText2: 'Get Free Consultation',
+    redirect: '/contact',
+    imageSrc: '/assets/ai-strategy/ai-strategy-hero-img.png',
+    imageAlt: 'AI Strategy',
+  },
+
+  featuresOfAiStrategy: {
+    title: 'Primathon’s AI Strategy Consulting Approach',
+    cardData: [
+      {
+        id: '1',
+        cardTitle: 'Enhance',
+        content:
+          'Our AI strategy consulting begins by identifying use cases to enhance your current processes through AI optimization.',
+      },
+      {
+        id: '2',
+        cardTitle: 'Propel',
+        content:
+          "We'll explore how AI solutions can propel your current strategic outcomes and areas of focus, accelerating your path to success.",
+      },
+      {
+        id: '3',
+        cardTitle: 'Revolutionize',
+        content:
+          'Our team, then revolutionize your approach to product development, customer satisfaction, and service delivery with AI-driven transformation.',
+      },
+    ],
+  },
+
+  aiStrategyServices: {
+    title: 'Our AI Strategy Consulting Services that Makes Us Standout',
+    cardData: [
+      {
+        id: 1,
+        imageSrc: '/assets/ai-strategy/ai-roadmap.svg',
+        cardTitle: 'Curate a Specific AI Roadmap',
+        content:
+          "We begin by conducting a thorough assessment of your organization's current state, including existing technologies, data assets, and business processes. This helps us understand your unique challenges, opportunities, and readiness for AI adoption. We also analyze AI market trends so that we align it with your business goals.",
+      },
+      {
+        id: 2,
+        imageSrc: '/assets/ai-strategy/innovative-solution.svg',
+        cardTitle: 'Innovative Solutions',
+        content:
+          "We pride ourselves on our ability to deliver innovative AI leadership solutions that push the boundaries of what's possible, helping our clients to stay ahead of the curve and maintain a competitive edge.",
+      },
+      {
+        id: 3,
+        imageSrc: '/assets/ai-strategy/implementation.svg',
+        cardTitle: 'Practical Implementation',
+        content:
+          "We don't just stop at strategic planning—we roll up our sleeves and work alongside your team to ensure seamless implementation of AI initiatives, driving tangible results and ROI.",
+      },
+      {
+        id: 4,
+        imageSrc: '/assets/ai-strategy/holistic-perspective.svg',
+        cardTitle: 'Holistic Perspective',
+        content:
+          'We take a holistic approach to AI strategy consulting, considering not only the technological aspects but also the organizational, cultural, and ethical dimensions, ensuring comprehensive and sustainable solutions.',
+      },
+      {
+        id: 5,
+        imageSrc: '/assets/ai-strategy/support.svg',
+        cardTitle: 'Personalized Support',
+        content:
+          'Efficiently incorporate strategic AI advancements into your business processes and organizational framework with tailored 1:1 guidance from our team of AI experts, ensuring seamless integration and optimal utilization.',
+      },
+      {
+        id: 6,
+        imageSrc: '/assets/ai-strategy/management.svg',
+        cardTitle: 'Project Management',
+        content:
+          "When it's time to build your artificial intelligence tech stack, tap into our extensive network of strategic partners to develop the AI technologies and customized solutions crucial for fostering your organization's growth.",
+      },
+    ],
+  },
+
+  ctaSection1: {
+    title: 'Are you Confused About the Right AI Development Path?',
+    description:
+      'Allow our team of AI discovery experts to lead your project, ensuring you make confident and informed decisions along the way.',
+    ctaText: 'Get Free Consultation',
+    redirect: '/contact',
+    bgColor: '#21315B',
+  },
+
+  needOfAiStrategy: {
+    title: 'Why Does Your Business Need an Artificial Intelligence Strategy Today?',
+    cardData: [
+      {
+        id: 1,
+        imageSrc: '/assets/ai-strategy/maximizes-roi.svg',
+        cardTitle: 'It Maximizes Your ROI',
+        cardData:
+          'Investing in AI without a clear strategy can be costly and ineffective. A well-defined AI strategic planning and roadmap enable you to prioritize investments, allocate resources effectively, and maximize ROI.',
+      },
+      {
+        id: 2,
+        imageSrc: '/assets/ai-strategy/business.svg',
+        cardTitle: 'Future-Proof Your Business',
+        cardData:
+          "As AI continues to advance at a rapid pace, organizations that fail to adopt AI risk falling behind and becoming obsolete. Developing an AI strategy and corporate AI integration roadmap now allows you to future-proof your business, ensuring that you're well-positioned to adapt to emerging trends, technologies, and market dynamics.",
+      },
+      {
+        id: 3,
+        imageSrc: '/assets/ai-strategy/consideration.svg',
+        cardTitle: 'Address AI Ethical Considerations',
+        cardData:
+          'By incorporating ethical principles and regulatory compliance into your AI strategy and roadmap now, you can mitigate risks, safeguard against potential liabilities, and build a reputation as a responsible and trustworthy organization.',
+      },
+      {
+        id: 4,
+        imageSrc: '/assets/ai-strategy/Productivity.svg',
+        cardTitle: 'Efficiency and Productivity',
+        cardData:
+          'AI technologies, such as automation and machine learning, have the potential to significantly improve efficiency and productivity within your organization. By automating repetitive tasks, streamlining workflows, and enabling predictive analytics, AI can free up valuable time and resources for more strategic endeavors.',
+      },
+    ],
+  },
+
+  benefitsOfAiStrategy: {
+    title: 'Benefits of Primathon’s AI Strategy Services',
+    cardData: [
+      {
+        id: 1,
+        imageSrc: '/assets/ai-strategy/alignment.svg',
+        cardTitle: 'Strategic Alignment',
+        content:
+          'Our AI strategy services ensure that your AI initiatives are closely aligned with your overall business strategy, helping you achieve your long-term goals and objectives.',
+      },
+      {
+        id: 2,
+        imageSrc: '/assets/ai-strategy/advantage.svg',
+        cardTitle: 'Competitive Advantage',
+        content:
+          'By using strategic AI technologies, you can gain a competitive edge in your industry, whether through improved efficiency, enhanced decision-making, or innovative product offerings.',
+      },
+      {
+        id: 3,
+        imageSrc: '/assets/ai-strategy/efficiency.svg',
+        cardTitle: 'Increased Efficiency',
+        content:
+          'Our AI strategic planning is designed to optimize your business processes, automating repetitive tasks, streamlining operations, and reducing manual effort, leading to increased productivity and cost savings.',
+      },
+      {
+        id: 4,
+        imageSrc: '/assets/ai-strategy/insight.svg',
+        cardTitle: 'Data-Driven Insights',
+        content:
+          'AI enables you to unlock valuable insights from your data, empowering you to make informed decisions, identify opportunities, and anticipate market trends with greater accuracy.',
+      },
+      {
+        id: 5,
+        imageSrc: '/assets/ai-strategy/innovation.svg',
+        cardTitle: 'Innovation',
+        content:
+          'With our AI strategy services, you can foster a culture of innovation within your organization, exploring new ideas, experimenting with emerging technologies, and driving continuous improvement across all aspects of your business.',
+      },
+      {
+        id: 6,
+        imageSrc: '/assets/ai-strategy/adaptability.svg',
+        cardTitle: 'Agility and Adaptability',
+        content:
+          "In today's fast-paced business environment, agility and adaptability are essential for success. Our AI strategy services enable you to respond quickly to changing market conditions, emerging opportunities, and evolving customer demands, positioning your organization for sustained growth and resilience.",
+      },
+    ],
+  },
+
+  whyChoose: {
+    title: 'Why Choose Primathon As Your AI Strategy Service Partner?',
+    description:
+      'We tailor your AI requirements according to your business needs for swift and efficient delivery, ensuring exceptional user experiences while adhering to project timelines and budget constraints.',
+    listItems: [
+      { id: 1, text: 'Experience 1.5X Faster Delivery' },
+      { id: 2, text: 'Work at Your Time Zone' },
+      { id: 3, text: 'Hassle-free communication' },
+      { id: 4, text: '24/7 Support' },
+    ],
+    service1: { number: '6+', text: 'Years of business' },
+    service2: { number: '80+', text: 'Software Developers' },
+    service3: { number: '150+', text: 'Project Delivered' },
+    service4: { number: '100+', text: 'Happy Client' },
+  },
+
+  trustedPartner: {
+    title: 'Primathon- Your Trusted Partner to Curate AI Strategy and Roadmap',
+    description:
+      "As the premier artificial intelligence consulting firm, we uniquely combine strategy expertise with AI specialists. We understand the plethora of options available for AI consulting services. However, our team stands out with unparalleled real-world experience and a pragmatic approach to strategy implementation. Moreover, we've collaborated with teams of technologists, developers, and project managers to turn your AI aspirations into tangible results. With us, your AI vision becomes a reality.",
+    imageSrc: '/assets/ai-strategy/trusted-partner.png',
+    alt: 'AI Strategy',
+  },
+
+  aiStrategyJourney: {
+    heading: 'Let us take your project on a journey!',
+    items: [
+      'You share your idea with us',
+      'We Explore AI Potential Analysis',
+      'Our Team Craft a Strategic AI Roadmap',
+      'we deliver on time',
+    ],
+  },
+};
