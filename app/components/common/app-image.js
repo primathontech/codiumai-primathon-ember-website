271e7c4f41c6572759700750be510e1966421048
Ramvishvas Kumar <ramvishvas.kumar@primathon.in>
2024-05-03 16:58:28 +0530

    feat: fix full url path check for image

--- app/components/common/app-image.js
+++ app/components/common/app-image.js
@@ -2,7 +2,16 @@ import Component from '@glimmer/component';
 import ENV from 'primathon-ember-web/config/environment';
 
 export default class CommonAppImageComponent extends Component {
+  isFullURL(url) {
+    // Regular expression pattern to match a full URL
+    const FULL_URL_PATTERN = /^(?:https?:\/\/)?(?:www\.)?\w+\.\w+/i;
+    return FULL_URL_PATTERN.test(url);
+  }
+
   get src() {
+    if (this.isFullURL(this.args.src)) {
+      return this.args.src;
+    }
     return ENV.ASSETS_CDN_URL ? `${ENV.ASSETS_CDN_URL}${this.args.src}` : this.args.src;
   }
 
@@ -12,6 +21,9 @@ export default class CommonAppImageComponent extends Component {
         .trim()
         .split(',')
         .map((src) => {
+          if (this.isFullURL(this.args.src)) {
+            return this.args.src;
+          }
           return ENV.ASSETS_CDN_URL ? `${ENV.ASSETS_CDN_URL}${src}` : src;
         });
       return srcsetArray.join(',');
